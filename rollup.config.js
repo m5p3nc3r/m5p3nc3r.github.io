c01c4d5c574fd7c399a38978dda5b749a25a73eb
Matt Spencer <matthew@thespencers.me.uk>
2020-02-12 18:12:30 +0000

    Minify javascript to speed up loading

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
+ import { terser } from "rollup-plugin-terser";
 
 export default [{
   input: ['assets/js/sources_mwc.js'],
@@ -6,12 +7,12 @@ export default [{
     dir: 'assets/bundle',
     format: 'cjs'
   },
-  plugins: [resolve()]
+  plugins: [resolve(), terser()]
 }, {
   input: ['assets/js/sources_polymer.js'],
   output: {
     dir: 'assets/bundle',
     format: 'cjs'
   },
-  plugins: [resolve()]  
+  plugins: [resolve(), terser()]
 }];
